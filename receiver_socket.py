776e919c71ac8d426eb561560a3cf77f9a420a4b
Priyank <priyankmakwana88@gmail.com>
2018-05-17 14:09:54 +0530

    May 17 try 1

--- receiver_socket.py
+++ receiver_socket.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import socket
 
-recv_ip='192.168.0.108'
+recv_ip='127.0.0.1'
 port=8888
 
 
@@ -11,5 +11,8 @@ s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind((recv_ip,port))
 
 while 1:
-	print (s.recvfrom(1000))
-
+	data_rec=s.recvfrom(1000)
+	msg_rec=data_rec[0]
+	print (msg_rec)
+	reply=raw_input("Enter your reply : ")
+	s.sendto(reply,(data_rec[1]))

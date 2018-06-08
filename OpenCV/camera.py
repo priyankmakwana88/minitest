7f03591ead245dda3e2ed8af16d0bdc7af0bc4ec
Priyank <priyankmakwana88@gmail.com>
2018-06-08 08:30:58 +0530

    june8 camera with blur

--- OpenCV/camera.py
+++ OpenCV/camera.py
@@ -11,7 +11,8 @@ cam=cv2.VideoCapture(0) #change to '1' if external webcam is attached along with
 while cam.isOpened():
 	status,frame=cam.read()
 	new=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-	cv2.imshow('cam0',frame)
+	gray=cv2.GaussianBlur(new, (21, 21), 0)
+	cv2.imshow('cam0',gray)
 	cv2.imshow('cam1',new)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break

d571d698980cb06e1e46722c1b8d0976ddcbdeb3
Priyank <priyankmakwana88@gmail.com>
2018-06-08 22:14:32 +0530

    june8 save image in .npy format

--- OpenCV/save_imgdata.py
+++ OpenCV/save_imgdata.py
@@ -13,11 +13,14 @@ cat5=cv2.imread('cat5.jpg',1)
 cat=[cat1,cat2,cat3,cat4,cat5]
 
 for i in range(0,len(cat)):
+	'''
 	heigth=cat[i].shape[0]
 	width=cat[i].shape[1]
 	bgr=cat[i].shape[2]
 	
 	factor=width*bgr
 	cat[i]=cat[i].reshape(heigth,factor)
-	np.savetxt('cat'+str(i+1)+'.txt',cat[i],fmt='%d',comments='reshaped data')
+	'''	
+	#np.savetxt('cat'+str(i+1)+'.txt',cat[i],fmt='%d',comments='reshaped data')
+	np.save('cat'+str(i),cat[i])
 
